dac29972753a77ea75e041acede7fb2fd3eb5f68
nawal-ali <nmaaa3899@gmail.com>
2025-02-14 20:50:15 +0300

    added navbar

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,13 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+
+// import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1>home page works!</h1>
+      <h1>app works!</h1>
     </>
   )
 }
