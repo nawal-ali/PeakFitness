60bff52951120f5f2b59325f86972c6aa19bcc05
nawal-ali <nmaaa3899@gmail.com>
2025-02-15 10:04:52 +0300

    changed the nav to dark

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,15 +1,14 @@
-import './App.css'
+import "./App.css";
 
-import Home from './pages/Home'
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
-    <Home/>
-      <h1>app works!</h1>
+      <Home />
+      {/* <h1>app works!</h1> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- project/src/assets/Navbar.jsx
+++ project/src/assets/Navbar.jsx
@@ -1,47 +1,97 @@
-export default function navbar(){
-    return(
-        <>
-<nav className="navbar bg-body-tertiary fixed-top">
-    <div className="container-fluid">
-    <a className="navbar-brand" href="#">Offcanvas navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-            </a>
-            <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li>
-                <hr className="dropdown-divider"/>
+export default function navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark fixed-top p-3">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Offcanvas dark navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+            aria-controls="offcanvasDarkNavbar"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="offcanvas offcanvas-end text-bg-dark"
+            tabIndex="-1"
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                Dark offcanvas
+              </h5>
+              <button
+                type="button"
+                className="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
                 </li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            </li>
-        </ul>
-        <form className="d-flex mt-3" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex mt-3" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-    </div>
-    </div>
-</nav>
-        </>
-    )
+      </nav>
+    </>
+  );
 }
