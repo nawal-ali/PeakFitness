08eba48d8c4ed395c21bc7d9c99258e81aeb09ff
nawal-ali <nmaaa3899@gmail.com>
2025-03-06 23:06:57 +0200

    done about us section

--- project/src/pages/aboutFolder/About.jsx
+++ project/src/pages/aboutFolder/About.jsx
@@ -1,12 +1,40 @@
 import Navbar from "../../assets/navFolder/Navbar";
 import Footer from "../../assets/footerFolder/Footer";
+import MainDiv from "./about-components/MainDiv";
+import IconsDiv from "./about-components/IconsDiv";
+import ChooseUs from "./about-components/ChooseUs";
 export default function about() {
   return (
     <>
-      <div className="vw-100 position-relative">
-        <Navbar />
+      <Navbar showBackground={false} />
+      <MainDiv />
+      <div className="row mt-5 w-100">
+        <div className="col-12">
+          <IconsDiv />
+        </div>
+        <div style={{ width: "90%", margin: "6% auto" }}>
+          <ChooseUs />
+        </div>
       </div>
-      <Footer />
+      <div className="row" style={{ width: "90%", margin: "6% auto" }}>
+        <div className="col-12 col-md-6">
+          <img src="./imgs/our-story.png" alt="" className="w-100 rounded" />
+        </div>
+        <div className="col-12 col-md-6">
+          <h1 style={{ color: "#EC7E4A" }}>OUR STORY</h1>
+          <p className="fs-2">
+            &quot;PeakFitness was born from a passion for strength, endurance,
+            and transformation. What started as a small training space has grown
+            into a community where individuals push limits, break barriers, and
+            achieve greatness. Here, every drop of sweat tells a story of
+            dedication, and every workout brings you closer to your best
+            self.&quot;
+          </p>
+        </div>
+      </div>
+      <div className="mt-5">
+        <Footer />
+      </div>
     </>
   );
 }
