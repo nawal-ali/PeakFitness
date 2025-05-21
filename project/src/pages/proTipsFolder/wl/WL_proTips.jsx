82532f49b2741c4a4dd93868cbaa3ada74850b39
OmarNabiil <168189924+OmarNabiil@users.noreply.github.com>
2025-05-21 18:48:02 +0300

    weight loss pro tips photos

--- project/src/pages/proTipsFolder/wl/WL_proTips.jsx
+++ project/src/pages/proTipsFolder/wl/WL_proTips.jsx
@@ -7,29 +7,29 @@ export default function WL_proTips() {
     <>
       <Navbar />
       <ProTipsTemp
-        sec1_tips_img="./imgs/wg_sec1_tips.png"
+        sec1_tips_img="./imgs/wl_sec1_tips.png"
         sec1_tips_title="Weight Loss"
         sec1_tips_desc="In this section, you’ll find expert tips to help you effectively and sustainably reach your weight loss goals"
         sec2_tips_img="./imgs/wg_tips_sec2.png"
         sec2_tips_title="Effective Fat-Burning Workouts"
         sec2_tips_list={[
           {
-            img: "./logo/mg_tips_sec2_logo1.svg",
+            img: "./logo/wl_tips_sec2_logo1.svg",
             coloredText: "Running or Brisk Walking",
             text: "Burns calories and increases heart rate.",
           },
           {
-            img: "./logo/mg_tips_sec2_logo1.svg",
+            img: "./logo/wl_tips_sec2_logo2.svg",
             coloredText: "HIIT",
             text: "Combines intense activity with short rest periods for fast fat burning.",
           },
           {
-            img: "./logo/mg_tips_sec2_logo1.svg",
+            img: "./logo/wl_tips_sec2_logo3.svg",
             coloredText: "Swimming",
             text: "A full-body workout that burns fat and builds muscle.",
           },
           {
-            img: "./logo/mg_tips_sec2_logo1.svg",
+            img: "./logo/wl_tips_sec2_logo4.svg",
             coloredText: "Strength Training",
             text: "Builds muscle, increasing fat burn even at rest.",
           },
@@ -43,10 +43,10 @@ export default function WL_proTips() {
           "Rich in protein, they support muscle building and increase fat burning.",
         ]}
         sec3_tips_imgs={[
-          "./imgs/mg_tips_sec31.png",
-          "./imgs/mg_tips_sec31.png",
-          "./imgs/mg_tips_sec31.png",
-          "./imgs/mg_tips_sec31.png",
+          "./imgs/wl_tips_sec31.png",
+          "./imgs/wl_tips_sec33.png",
+          "./imgs/wl_tips_sec34.png",
+          "./imgs/wl_tips_sec32.png",
         ]}
         sec3_tips_coloredText={[
           "Green Tea",
@@ -55,7 +55,7 @@ export default function WL_proTips() {
           "Eggs",
         ]}
         sec4_tips_title="Water's Role in Weight Loss"
-        sec4_tips_img="./imgs/wg_tips_sec4.png"
+        sec4_tips_img="./imgs/wl_tips_sec4.png"
         sec4_tips_list={[
           "Curbs Appetite: Drinking water before meals reduces hunger.",
           "Boosts Calorie Burn: Water enhances metabolism and burns more calories.",
