f2bdc1dc697a2070070b5edadebe1fb5e10bc5fa
nawal-ali <nmaaa3899@gmail.com>
2025-06-21 23:21:21 +0300

    fixed pro tips sec 3 errors

--- project/src/pages/proTipsFolder/proTips_template/components/sec1_tips/Sec1_tips.jsx
+++ project/src/pages/proTipsFolder/proTips_template/components/sec1_tips/Sec1_tips.jsx
@@ -6,7 +6,7 @@ export default function Sec1_tips({
   return (
     <>
       <div className="row">
-        <div className="col-12 col-md-5 d-flex justify-content-center align-items-center ps-5">
+        <div className="col-12 order-2 order-md-1 col-md-5 d-flex justify-content-center align-items-center ps-5">
           <div>
             <h1 style={{ color: "#CB8778", fontSize: "6rem" }}>
               {sec1_tips_title}
@@ -14,7 +14,7 @@ export default function Sec1_tips({
             <h3 className="fw-bold">{sec1_tips_desc}</h3>
           </div>
         </div>
-        <div className="col-12 col-md-7">
+        <div className="col-12 order-1 order-md-2 col-md-7">
           <img src={sec1_tips_img} className="w-100" alt="" />
         </div>
       </div>
